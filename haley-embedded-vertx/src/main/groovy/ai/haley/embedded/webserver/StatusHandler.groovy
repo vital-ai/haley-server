067964dd24bafc3c2aa987ba1100a7adead85552
Derek Kobylarz <darek.kobylarz@gmail.com>
2018-01-26 13:34:06 +0100

    status endpoint mem/disk check

--- haley-embedded-vertx/src/main/groovy/ai/haley/embedded/webserver/StatusHandler.groovy
+++ haley-embedded-vertx/src/main/groovy/ai/haley/embedded/webserver/StatusHandler.groovy
@@ -3,18 +3,32 @@ package ai.haley.embedded.webserver
 import groovy.json.JsonOutput
 import io.vertx.core.Handler
 import io.vertx.groovy.ext.web.RoutingContext
+
+import java.text.DecimalFormat;
+
 import ai.haley.embedded.HaleyEmbeddedApp
 
 class StatusHandler implements Handler<RoutingContext> {
 
 	public final static String pingScript = 'commons/scripts/ServicesAccessScript.groovy'
 	
+	static double mib = 1024d*1024d;
 	
+	static double gib = 1024d*1024d*1024d;
+	
+	static DecimalFormat df = new DecimalFormat("0.0");
 	
 	@Override
 	public void handle(RoutingContext ctx) {
 
-		Map r = [ok: true, message: 'OK']
+		boolean ok = true
+		
+		String message = ''
+		
+		def sendResponse = { ->
+		Map r = [ok: ok, message: message]
+			ctx.response().end(JsonOutput.toJson(r), 'UTF-8')
+		}
 		
 		if(HaleyEmbeddedApp.sendAssetConditionMessage || HaleyEmbeddedApp.sendAssetLocationMessage) {
 			
@@ -27,8 +41,10 @@ class StatusHandler implements Handler<RoutingContext> {
 			int ago = (int) ( ( System.currentTimeMillis() - ts ) / 1000L ) 
 			
 			if(ago > margin) {
-				r.ok = false
-				r.message = "Last temp/location message was sent ${ago} seconds ago, interval ${interval}"
+				ok = false
+				message = "Last temp/location message was sent ${ago} seconds ago, interval ${interval}"
+				sendResponse()
+				return
 			}
 
 			ts = HaleyEmbeddedApp.lastAssetReceivedTimestamp
@@ -36,14 +52,49 @@ class StatusHandler implements Handler<RoutingContext> {
 			ago = (int) ( ( System.currentTimeMillis() - ts ) / 1000L )
 			
 			if(ago > margin) {
-				r.ok = false
-				r.message = "Last temp/location message data was received ${ago} seconds ago, interval ${interval}"
+				ok = false
+				message = "Last temp/location message data was received ${ago} seconds ago, interval ${interval}"
+				sendResponse()
+				return
 			} 
-						
-		} 
+			
+		}
+		
+		message = 'Data OK'
+		
+		Runtime runtime = Runtime.getRuntime();
+		
+		double maxMemory = (double) runtime.maxMemory();
+	
+		double usedMemory = (double) ( runtime.totalMemory() - runtime.freeMemory() );
+	
+		double memUsage = ( usedMemory / maxMemory ) * 100d;
+	
+		if(memUsage > 90d) {
+			ok = false;
+			message += ( "\n available memory below 10% (" + df.format(memUsage) + "%) - used " + df.format(usedMemory/mib) + "MiB, max: " + df.format(maxMemory/mib) + "MiB" );
+		} else {
+			message += ( "\n mem usage OK: " + df.format(memUsage) + "% - used " + df.format(usedMemory/mib) + "MiB, max: " + df.format(maxMemory/mib) + "MiB" );
+		}
+
+	
+		File file = new File(".");
+		double totalSpace = file.getTotalSpace();
+//		double freeSpace = file.getFreeSpace();
+		double usableSpace = file.getUsableSpace();
+		double usedSpace = totalSpace - usableSpace;
+
+		double diskUsage = (usedSpace / totalSpace) * 100d;
+		
+		if(diskUsage > 90d) {
+			ok = false;
+			message += ( "\n available disk space below 10% (" + df.format(diskUsage) + "%) - used " + df.format(usedSpace/gib) + "GiB, total: " + df.format(totalSpace/gib) + "GiB");
+		} else {
+			message += ( "\n disk usage OK: " + df.format(diskUsage) + "% - used " + df.format(usedSpace/gib) + "GiB, total: " + df.format(totalSpace/gib) + "GiB");
+		}
 		
 		
-		ctx.response().end(JsonOutput.toJson(r), 'UTF-8')
+		sendResponse()
 		
 	}
 
